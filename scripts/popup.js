b0658f895903bfd02990dae2c50e65159a8f43f2
Aleksandr Fedin <falv82@yandex.com>
2021-02-26 23:23:49 -0500

    курсовая 4 вар2

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,28 +1,27 @@
 let openPopupButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__close');
+let submitButton = document.querySelector('.popup__savebutton');
+let formElement = document.querySelector ('.popup__container');
 let nameInput = document.querySelector('.popup__field-name');
 let jobInput = document.querySelector('.popup__field-achievement');
+
 
 function openPopup() {
     popup.classList.add('popup_opened');
 }
 
-openPopupButton.addEventListener('click', openPopup);
-
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-closePopupButton.addEventListener('click', closePopup);
-
-
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    nameInput.textContent = document.querySelector('.profile__title').value;
-    jobInput.textContent = document.querySelector('.profile__subtitle').value;
+    nameInput.value = document.querySelector('.profile__title').textContent;
+    jobInput.value = document.querySelector('.profile__subtitle').textContent;
+    popup.classList.remove('popup_opened');
 }
 
-popup.addEventListener('submit', formSubmitHandler)
-
-
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
+formElement.addEventListener('click', formSubmitHandler);
