aa5a19067a756433e26643a5031e5907f0841602
Aleksandr Fedin <falv82@yandex.com>
2021-02-28 14:11:23 -0500

    курсовая 4 вер4

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,27 +1,40 @@
-let openPopupButton = document.querySelector('.profile__edit-button');
+//Записываем необходимые элементы в переменные
 let popup = document.querySelector('.popup');
+let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close');
-let submitButton = document.querySelector('.popup__savebutton');
+let submitPopupButton = document.querySelector('.popup__save-button');
+let nameInput = document.querySelector('.popup__field.popup__field_type_name');
+let jobInput = document.querySelector('.popup__field.popup__field_type_achievement');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector ('.popup__container');
-let nameInput = document.querySelector('.popup__field-name');
-let jobInput = document.querySelector('.popup__field-achievement');
 
-
+// Пишем обработчики, которые будут применяться при:
+//Клике  на кнопку "редактировать"
 function openPopup() {
     popup.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
 }
 
+//Клике  на кнопку "закрыть"
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
+//Внесении данных в поля формы
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    nameInput.value = document.querySelector('.profile__title').textContent;
-    jobInput.value = document.querySelector('.profile__subtitle').textContent;
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
     popup.classList.remove('popup_opened');
 }
 
+//Вешаем слушателя на кнопки
+//"редактировать"
 openPopupButton.addEventListener('click', openPopup);
+//"закрыть"
 closePopupButton.addEventListener('click', closePopup);
-formElement.addEventListener('click', formSubmitHandler);
+
+//Вешаем слушателя на внесении данных в поля формы
+formElement.addEventListener('submit', formSubmitHandler);
